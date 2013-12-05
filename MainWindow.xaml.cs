e3def21b0d4347da13d142e7e51f9e20e51370ef
wliu6v <wliu6v@163.com>
2013-12-05 17:24:50 +0800

    添加了键盘响应事件

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,17 +32,110 @@ namespace DailyReportAssistant
             textBox4.Text = GlobalVar.dailyReport[3];
             textBox5.Text = GlobalVar.dailyReport[4];
 
+            textBox1.Focus();
         }
 
         private void btnOK_Click(object sender, RoutedEventArgs e)
         {
-            String[] texts = { textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text};
-
+            String[] texts = {textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text};
             Transcation.Write(texts);
+            Environment.Exit(Environment.ExitCode);
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            
+            ((TextBox)sender).Select(0, ((TextBox)sender).Text.Length);
+        }
+
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key==Key.Enter)
+            {
+                btnOK_Click(sender, e);
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                btnCancel_Click(sender, e);
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                textBox2.Focus();
+            }
+        }
+
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.Enter)
+            {
+                btnOK_Click(sender, e);
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                btnCancel_Click(sender, e);
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                textBox3.Focus();
+            }
+        }
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.Enter)
+            {
+                btnOK_Click(sender, e);
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                btnCancel_Click(sender, e);
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                textBox4.Focus();
+            }
+        }
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.Enter)
+            {
+                btnOK_Click(sender, e);
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                btnCancel_Click(sender, e);
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                textBox5.Focus();
+            }
+        }
+        private void textBox5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control && e.Key == Key.Enter)
+            {
+                btnOK_Click(sender, e);
+            }
+
+            if (e.Key == Key.Escape)
+            {
+                btnCancel_Click(sender, e);
+            }
 
         }
     }
